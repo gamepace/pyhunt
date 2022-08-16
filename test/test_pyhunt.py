5ac178993bf2cd058549445bd0f062c2a7eb369d
Kilian <sadq@web.de>
2022-08-16 08:59:13 +0200

    Added several test functions

--- test/test_pyhunt.py
+++ test/test_pyhunt.py
@@ -14,17 +14,36 @@ def test_parseAttributesFile():
         level = -1
         
     try:
-        profileid = int(result['MissionBagPlayer_0_0_profileid']) 
+        profileId = int(result['MissionBagPlayer_0_0_profileid']) 
     except:
         print('Could not parse profileid')
-        profileid = -1    
+        profileId = -1    
         
-    assert isinstance(result, dict) and len(result.keys()) > 0 and level >= 0 and level <= 100 and profileid >= 0
+    assert isinstance(result, dict) and len(result.keys()) > 0 and level >= 0 and level <= 100 and profileId >= 0
 
 # parseMatchupFromAttributes
-
+def test_parseMatchupFromAttributes():
+    assert True
 
 # getAttributesFileHash
-
+def test_getAttributesFileHash():
+    hunt = pyhunt()
+    
+    # MD5
+    md5 = hunt.getAttributesFileHash(hunt._workingAttributesPath, 'md5')
+    # SHA256
+    sha256 = hunt.getAttributesFileHash(hunt._workingAttributesPath, 'sha256')
+    # Issue
+    unknown = hunt.getAttributesFileHash(hunt._workingAttributesPath, 'Not an Algo.')
+    
+    assert isinstance(md5, str) and isinstance(sha256, str) and len(md5) == 32 and len(sha256) == 64 and unknown is None 
 
 # copyAttributesToWorkPath
+def test_copyAttributesToWorkPath():
+    hunt = pyhunt()
+    srceHash = hunt.getAttributesFileHash(hunt._attributesPath)
+    hunt.copyAttributesToWorkPath()
+    copyHash = hunt.getAttributesFileHash(hunt._workingAttributesPath)
+    
+    assert isinstance(srceHash, str) and isinstance(copyHash, str) and srceHash == copyHash
+    
